1259bd3882c1c7459a1c23f2a9af32e0c2a20751
lucasgonzaaga <lucasgonzagasantoss020106@gmail.com>
2025-11-27 12:31:54 -0300

    corrigindo vite.js (ajustando o limite de chunk)

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <main className="bg-[var(--color-bg)] min-h-screen text-[var(--color-text)] transition-colors duration-500">
-      <CustomCursor theme={theme} />
+      {/* <CustomCursor theme={theme} /> */}
       <Navbar theme={theme} toggleTheme={toggleTheme} />
       {/* <Hero theme={theme} /> */}
       <About />
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 2000,
+    chunkSizeWarningLimit: 3000,
     rollupOptions: {
       output: {
         manualChunks: {
+          'vendor': ['react', 'react-dom', 'framer-motion'],
           'three': ['three', '@react-three/fiber', '@react-three/drei']
         }
       }
